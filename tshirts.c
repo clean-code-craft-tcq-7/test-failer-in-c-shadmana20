87dca65c6b69ccabc0779dd1eacd7158793c367c
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2022-12-06 10:44:32 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -14,7 +14,7 @@ char size(int cms) {
 }
 
 int main() {
-    assert(size(38) == 'S' || size(38) == 'M');
+    assert(size(38) == 'S');
     assert(size(37) == 'S');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
