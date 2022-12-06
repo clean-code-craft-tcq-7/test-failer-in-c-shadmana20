00743cb0c6bc7b8bf371550e5303b8e754a2d657
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2022-12-06 11:15:31 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -6,25 +6,53 @@
 int alertFailureCount = 0;
 int Current_Env = INTEGRATION_ENV;
 
+float convertfarenheittocelcius(float farenheit)
+{
+    float celcius = (farenheit - 32) * 5 / 9;
+    return celcius;
+}
+
+int networkAlertProduction(float celcius){
+  if(celcius<=200)
+  {
+     return 200;
+  }
+  else
+  {
+    return 500;
+  }
+}  
+
+float SelectAlertEnvironment(float celcius, Current_Env)
+{
+   float calculated_Value;
+   if(Current_Env == INTEGRATION_ENV )
+   {
+     calculated_Value = networkAlertProduction(celcius);
+   }
+   else
+   {
+     calculated_Value = networkAlertStub(celcius);
+   }
+   return calculated_Value;
+}
+
+
 int networkAlertStub(float celcius) {
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
-    // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    return 200;
+    if(celcius<=200)
+    {
+     return 200;
+     }
+    else
+    {
+     return 500;
+    }
 }
 
 void alertInCelcius(float farenheit) {
-    float celcius = (farenheit - 32) * 5 / 9;
-    int returnCode;
-    if(Current_Env == INTEGRATION_ENV)
-    {
-         returnCode = (int)celcius;
-    }
-    else
-    {
-         returnCode = networkAlertStub(celcius);
-    }
+    float celcius  = convertfarenheittocelcius(farenheit);
+    int returnCode = SelectAlertEnvironment(celcius,Current_Env);
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
@@ -35,9 +63,14 @@ void alertInCelcius(float farenheit) {
 }
 
 int main() {
-    alertInCelcius(400.5);
-    alertInCelcius(303.6);
-    assert(alertFailureCount == 2); 
+    alertInCelcius(380.5,Test);
+    assert(alertFailureCount == 0);
+    alertInCelcius(303.6,Production);
+    assert(alertFailureCount == 0);
+    alertInCelcius(515.5,Production);
+    assert(alertFailureCount == 1);
+    alertInCelcius(515.5,Test);
+    assert(alertFailureCount == 2);
     printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
     return 0;
