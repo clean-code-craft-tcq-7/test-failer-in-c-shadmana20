e7f88a5b22bf0256d42e3bf740a18eb5f588b5d5
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2022-12-06 11:07:10 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -18,8 +18,7 @@ int main() {
     assert(size(37) == 'S');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
-    assert(size(-1) == '\0');
-    assert(size(38) != '\0');
+   
     printf("All is well (maybe!)\n");
     return 0;
 }
