59b7759512b275fce87b644181f8c8745d244ccb
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2022-12-06 11:47:13 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -60,7 +60,7 @@ void alertInCelcius(float farenheit,int Current_Env) {
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
+        alertFailureCount += 1;
     }
 }
 
